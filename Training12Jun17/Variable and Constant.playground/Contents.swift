87c0591c1d6be727535e34d646b20f96a1594380
Student05 <sombat@snru.ac.th>
2017-06-12 14:23:08 +0700

    variable' ;

---
+++ Training12Jun17/Variable and Constant.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can pl
+import CoreAudioKit
+
+
+//My First program
+//Can not Change values
+    let myConstant=123;  //implicit Constant, assign value by user and
+                         //assign type by xcode=Constan, Cannot change value
+var intNumber=123;       //Implicigt variable intNumber=123, you can change value;
+
+//Explicit variable
+let myInt: Int =123;
+
+
+
+
+
+
+
+
+
+
+
+
